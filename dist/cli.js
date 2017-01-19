71220ded4a6bccd2d992999fe02737df4a4a9fe9
Paolo Ferretti <paolo.ferretti@contactlab.com>
2017-01-19 15:34:58 +0100

    CLI command enahncements
    - added meow and has-flags for command line arguments parsing
    - added bump api in main kompozer tool
    - removed old bump command line tool

--- dist/cli.js
+++ dist/cli.js
@@ -5,51 +5,65 @@ var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
 
+var _meow = require('meow');
+
+var _meow2 = _interopRequireDefault(_meow);
+
+var _hasFlag = require('has-flag');
+
+var _hasFlag2 = _interopRequireDefault(_hasFlag);
+
 var _index = require('./index');
 
 var _index2 = _interopRequireDefault(_index);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var config = require(_path2.default.resolve('clab-builder.conf'));
+var config = require(_path2.default.resolve('kubozer.conf'));
 var webpackConfig = require(_path2.default.resolve('webpack.config'));
 
 var NODE_ENV = process.env.NODE_ENV;
 
 var isProduction = function isProduction() {
-  return NODE_ENV === 'production';
+	return NODE_ENV === 'production';
 };
 
 var isStaging = function isStaging() {
-  return NODE_ENV === 'staging';
+	return NODE_ENV === 'staging';
 };
 
 var k = new _index2.default(config, webpackConfig);
 
+var cli = (0, _meow2.default)('\n\tUsage\n\t\t$ NODE_ENV=env_name kubozer --build\n\t\t$ NODE_ENV=env_name kubozer --bump semverlabel\n\n\tOptions\n\t\t--bump Semver label for version bump: patch, minor, major, prepatch, preminor, premajor, prerelease\n\n\tExamples\n\t\t$ NODE_ENV=staging kubozer --build\n\t\t$ NODE_ENV=staging kubozer --bump minor\n\n');
+
 var buildStaging = function buildStaging() {
-  k.deletePrevBuild(function () {});
-  k.copy().then(function () {
-    return k.build();
-  });
+	k.deletePrevBuild(function () {});
+	k.copy().then(function () {
+		return k.build();
+	});
 };
 
 var buildProduction = function buildProduction() {
-  k.deletePrevBuild(function () {});
-  k.copy().then(function () {
-    return k.build();
-  }).then(function () {
-    return k.minify();
-  });
+	k.deletePrevBuild(function () {});
+	k.copy().then(function () {
+		return k.build();
+	}).then(function () {
+		return k.minify();
+	});
 };
 
 var main = function main() {
-  if (isStaging()) {
-    buildStaging();
-  }
+	if (isStaging() && (0, _hasFlag2.default)('build')) {
+		buildStaging();
+	}
 
-  if (isProduction()) {
-    buildProduction();
-  }
+	if (isProduction() && (0, _hasFlag2.default)('build')) {
+		buildProduction();
+	}
+
+	if ((0, _hasFlag2.default)('bump')) {
+		k.bump(cli.flags.bump);
+	}
 };
 
 main();
--- src/cli.js
+++ src/cli.js
@@ -1,44 +1,64 @@
 #! /usr/bin/env node
 
 import path from 'path';
+import meow from 'meow';
+import hasFlag from 'has-flag';
 
 import Kubozer from './index';
 
-const config = require(path.resolve('clab-builder.conf'));
+const config = require(path.resolve('kubozer.conf'));
 const webpackConfig = require(path.resolve('webpack.config'));
 
 const NODE_ENV = process.env.NODE_ENV;
 
 const isProduction = () => {
-  return NODE_ENV === 'production';
+	return NODE_ENV === 'production';
 };
 
 const isStaging = () => {
-  return NODE_ENV === 'staging';
+	return NODE_ENV === 'staging';
 };
 
 const k = new Kubozer(config, webpackConfig);
 
+const cli = meow(`
+	Usage
+		$ NODE_ENV=env_name kubozer --build
+		$ NODE_ENV=env_name kubozer --bump semverlabel
+
+	Options
+		--bump Semver label for version bump: patch, minor, major, prepatch, preminor, premajor, prerelease
+
+	Examples
+		$ NODE_ENV=staging kubozer --build
+		$ NODE_ENV=staging kubozer --bump minor
+
+`);
+
 const buildStaging = () => {
-  k.deletePrevBuild(() => {});
-  k.copy().then(() => k.build());
+	k.deletePrevBuild(() => {});
+	k.copy().then(() => k.build());
 };
 
 const buildProduction = () => {
-  k.deletePrevBuild(() => {});
-  k.copy()
-    .then(() => k.build())
-    .then(() => k.minify());
+	k.deletePrevBuild(() => {});
+	k.copy()
+		.then(() => k.build())
+		.then(() => k.minify());
 };
 
 const main = () => {
-  if (isStaging()) {
-    buildStaging();
-  }
+	if (isStaging() && hasFlag('build')) {
+		buildStaging();
+	}
 
-  if (isProduction()) {
-    buildProduction();
-  }
+	if (isProduction() && hasFlag('build')) {
+		buildProduction();
+	}
+
+	if (hasFlag('bump')) {
+		k.bump(cli.flags.bump);
+	}
 };
 
 main();
