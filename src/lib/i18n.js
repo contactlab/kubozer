c0ed418821c8b0c9d594605dec3f03f56b956519
Paolo Ferretti <paolo@paoloferretti.net>
2017-03-13 16:42:18 +0100

    OneSky integration: download with promise

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -32,7 +32,7 @@ export default class OneSky {
     return new Promise((resolve, reject) => {
       fs.readFile(filePath, 'utf-8', (err, data) => {
         if (err) {
-          reject(err);
+          return reject(err);
         }
 
         const options = Object.assign({}, this.baseOptions, {
@@ -43,9 +43,9 @@ export default class OneSky {
           format: config.i18n.format
         });
         onesky.postFile(options).then(content => {
-          resolve(content);
+          return resolve(content);
         }).catch(error => {
-          reject(error);
+          return reject(error);
         });
       });
     });
@@ -54,24 +54,26 @@ export default class OneSky {
   download(languages) {
     const langs = this.parseLanguages(languages);
 
-    langs.forEach(language => {
-      const options = Object.assign({}, this.options, {
-        language: language,
-        fileName: 'EN.json'
-      });
-
-      onesky.getFile(options).then(content => {
-        const filePath = this.getFilePath(config.i18n.languagesPath, language);
-
-        fs.writeFile(filePath, content, err => {
-          if(err) {
-            console.error(err);
-            return;
-          }
-          console.info(language + ' translation succesfully downloaded');
+    return new Promise((resolve, reject) => {
+      langs.forEach(language => {
+        const options = Object.assign({}, this.options, {
+          language: language,
+          fileName: 'EN.json'
         });
-      }).catch(error => {
-        console.error(error.message);
+
+        onesky.getFile(options).then(content => {
+          const filePath = this.getFilePath(config.i18n.languagesPath, language);
+
+          fs.writeFile(filePath, content, err => {
+            if(err) {
+              return reject(err);
+            }
+
+            return resolve(content);
+          });
+        }).catch(error => {
+          return reject(error);
+        });
       });
     });
   };
