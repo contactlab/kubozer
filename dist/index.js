22bd992edb2b843ac884eb554033fed84aabff40
StefanoMagrassi <stefano.magrassi@gmail.com>
2017-05-12 12:21:14 +0200

    editorconfig -> fix files in dist

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -39,287 +39,287 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Kubozer = function () {
-	function Kubozer(config, webpackConfig) {
-		_classCallCheck(this, Kubozer);
+  function Kubozer(config, webpackConfig) {
+    _classCallCheck(this, Kubozer);
 
-		if (!config || !webpackConfig) {
-			throw new Error('Missing configurations.');
-		}
+    if (!config || !webpackConfig) {
+      throw new Error('Missing configurations.');
+    }
 
-		this.config = config;
-		this.webpackConfig = webpackConfig;
-		this._checkForRequired();
+    this.config = config;
+    this.webpackConfig = webpackConfig;
+    this._checkForRequired();
 
-		this.Builder = new _builder2.default(this.config, this.webpackConfig, this._res);
-		this.Minifier = new _minifier2.default(this.config);
+    this.Builder = new _builder2.default(this.config, this.webpackConfig, this._res);
+    this.Minifier = new _minifier2.default(this.config);
 
-		// Ensure no previous workspaces are present
-		this.deleteWorkspace();
-	}
+    // Ensure no previous workspaces are present
+    this.deleteWorkspace();
+  }
 
-	_createClass(Kubozer, [{
-		key: 'deleteWorkspace',
-		value: function deleteWorkspace() {
-			try {
-				_fsExtra2.default.removeSync(_path2.default.resolve(this.config.workspace));
-			} catch (err) {
-				throw new Error(err);
-			}
-		}
-	}, {
-		key: 'deletePrevBuild',
-		value: function deletePrevBuild() {
-			try {
-				var pathToBuild = _path2.default.resolve(this.config.buildFolder);
-				_fsExtra2.default.removeSync(_path2.default.resolve(pathToBuild));
-			} catch (err) {
-				throw new Error(err);
-			}
-		}
+  _createClass(Kubozer, [{
+    key: 'deleteWorkspace',
+    value: function deleteWorkspace() {
+      try {
+        _fsExtra2.default.removeSync(_path2.default.resolve(this.config.workspace));
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+  }, {
+    key: 'deletePrevBuild',
+    value: function deletePrevBuild() {
+      try {
+        var pathToBuild = _path2.default.resolve(this.config.buildFolder);
+        _fsExtra2.default.removeSync(_path2.default.resolve(pathToBuild));
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
 
-		/**
-   * {string} type: 'assets' || 'bundles'
-   *
-   */
+    /**
+     * {string} type: 'assets' || 'bundles'
+     *
+     */
 
-	}, {
-		key: 'copy',
-		value: function copy() {
-			var _this = this;
+  }, {
+    key: 'copy',
+    value: function copy() {
+      var _this = this;
 
-			this._ensureWorkspace();
+      this._ensureWorkspace();
 
-			return new Promise(function (resolve, reject) {
-				if (_this.config.manifest) {
-					_this._copyManifest();
-				}
+      return new Promise(function (resolve, reject) {
+        if (_this.config.manifest) {
+          _this._copyManifest();
+        }
 
-				if (_this.config.copy) {
-					_this.config.copy.forEach(function (type) {
-						type.items.forEach(function (item) {
-							try {
-								var itemPath = _path2.default.join(_path2.default.resolve(_this.config.workspace), type.baseFolder, item);
-								var destination = _path2.default.join(_path2.default.resolve(_this.config.buildFolder), type.baseFolder, item);
+        if (_this.config.copy) {
+          _this.config.copy.forEach(function (type) {
+            type.items.forEach(function (item) {
+              try {
+                var itemPath = _path2.default.join(_path2.default.resolve(_this.config.workspace), type.baseFolder, item);
+                var destination = _path2.default.join(_path2.default.resolve(_this.config.buildFolder), type.baseFolder, item);
 
-								_fsExtra2.default.copySync(itemPath, destination);
-								return resolve(_this._res(undefined, { itemPath: itemPath, destination: destination }, 'Copy completed.'));
-							} catch (err) {
-								return reject(err);
-							}
-						});
-					});
-				}
+                _fsExtra2.default.copySync(itemPath, destination);
+                return resolve(_this._res(undefined, { itemPath: itemPath, destination: destination }, 'Copy completed.'));
+              } catch (err) {
+                return reject(err);
+              }
+            });
+          });
+        }
 
-				// If "copy" is empty
-				reject(_this._res(true, undefined, 'copy() method was called but "copy" property is empty or undefined.'));
-			});
-		}
-	}, {
-		key: 'replace',
-		value: function replace() {
-			var _this2 = this;
+        // If "copy" is empty
+        reject(_this._res(true, undefined, 'copy() method was called but "copy" property is empty or undefined.'));
+      });
+    }
+  }, {
+    key: 'replace',
+    value: function replace() {
+      var _this2 = this;
 
-			this._ensureWorkspace();
+      this._ensureWorkspace();
 
-			var optionCSS = {};
-			var optionJS = {};
+      var optionCSS = {};
+      var optionJS = {};
 
-			if (this.config.replace && this.config.replace.css) {
-				var cssFiles = this.config.replace.css.files;
-				optionCSS.files = _path2.default.join(_path2.default.resolve(this.config.workspace), cssFiles);
-				optionCSS.from = this.config.replace.css.commentRegex.map(function (item) {
-					return new RegExp(item, 'g');
-				});
-				optionCSS.to = this.config.replace.css.with.map(function (item) {
-					return '\n\t\t\t\t<link rel="stylesheet" href="' + item + '" />\n\t\t\t\t';
-				});
-			}
+      if (this.config.replace && this.config.replace.css) {
+        var cssFiles = this.config.replace.css.files;
+        optionCSS.files = _path2.default.join(_path2.default.resolve(this.config.workspace), cssFiles);
+        optionCSS.from = this.config.replace.css.commentRegex.map(function (item) {
+          return new RegExp(item, 'g');
+        });
+        optionCSS.to = this.config.replace.css.with.map(function (item) {
+          return '\n        <link rel="stylesheet" href="' + item + '" />\n        ';
+        });
+      }
 
-			if (this.config.replace && this.config.replace.js) {
-				optionJS.files = _path2.default.join(_path2.default.resolve(this.config.workspace), this.config.replace.js.files);
-				optionJS.from = this.config.replace.js.commentRegex.map(function (item) {
-					return new RegExp(item, 'g');
-				});
-				optionJS.to = this.config.replace.js.with.map(function (item) {
-					return '\n\t\t\t\t<script src="' + item + '"></script>\n\t\t\t\t';
-				});
-			}
+      if (this.config.replace && this.config.replace.js) {
+        optionJS.files = _path2.default.join(_path2.default.resolve(this.config.workspace), this.config.replace.js.files);
+        optionJS.from = this.config.replace.js.commentRegex.map(function (item) {
+          return new RegExp(item, 'g');
+        });
+        optionJS.to = this.config.replace.js.with.map(function (item) {
+          return '\n        <script src="' + item + '"></script>\n        ';
+        });
+      }
 
-			if (optionJS.files === undefined && optionCSS.files === undefined) {
-				throw new Error('WARNING REPLACE(): replace method called but "files" not found in configuration');
-			}
+      if (optionJS.files === undefined && optionCSS.files === undefined) {
+        throw new Error('WARNING REPLACE(): replace method called but "files" not found in configuration');
+      }
 
-			return new Promise(function (resolve, reject) {
-				try {
-					var changedCSS = _replaceInFile2.default.sync(optionCSS);
-					var changedJS = _replaceInFile2.default.sync(optionJS);
-					return resolve(_this2._res(undefined, { changedCSS: changedCSS, changedJS: changedJS }, 'Replace-in-file completed.'));
-				} catch (err) {
-					reject(_this2._res(true, undefined, err));
-				}
-			});
-		}
-	}, {
-		key: 'build',
-		value: function build(minify) {
-			var _this3 = this;
+      return new Promise(function (resolve, reject) {
+        try {
+          var changedCSS = _replaceInFile2.default.sync(optionCSS);
+          var changedJS = _replaceInFile2.default.sync(optionJS);
+          return resolve(_this2._res(undefined, { changedCSS: changedCSS, changedJS: changedJS }, 'Replace-in-file completed.'));
+        } catch (err) {
+          reject(_this2._res(true, undefined, err));
+        }
+      });
+    }
+  }, {
+    key: 'build',
+    value: function build(minify) {
+      var _this3 = this;
 
-			this._ensureWorkspace();
+      this._ensureWorkspace();
 
-			var resWebpack = void 0;
-			var resVulcanize = void 0;
+      var resWebpack = void 0;
+      var resVulcanize = void 0;
 
-			return this.Builder.webpack(minify).then(function (res) {
-				resWebpack = res;
+      return this.Builder.webpack(minify).then(function (res) {
+        resWebpack = res;
 
-				var promises = [_this3.Builder.vulcanize()];
-				if (minify) {
-					promises = promises.concat(_this3.Minifier.minifyCSS());
-				}
+        var promises = [_this3.Builder.vulcanize()];
+        if (minify) {
+          promises = promises.concat(_this3.Minifier.minifyCSS());
+        }
 
-				return Promise.all(promises);
-			}).then(function (res) {
-				resVulcanize = res[0];
-				var dataReturn = {
-					resWebpack: resWebpack,
-					resVulcanize: resVulcanize
-				};
-				// Present only with `minify` true or just undefined
-				var resMinifiedCss = res[1];
-				if (resMinifiedCss) {
-					dataReturn.resMinifiedCss = resMinifiedCss;
-				}
+        return Promise.all(promises);
+      }).then(function (res) {
+        resVulcanize = res[0];
+        var dataReturn = {
+          resWebpack: resWebpack,
+          resVulcanize: resVulcanize
+        };
+        // Present only with `minify` true or just undefined
+        var resMinifiedCss = res[1];
+        if (resMinifiedCss) {
+          dataReturn.resMinifiedCss = resMinifiedCss;
+        }
 
-				return dataReturn;
-			}).catch(function (err) {
-				throw err;
-			});
-		}
-	}, {
-		key: 'bump',
-		value: function bump(type) {
-			var _this4 = this;
+        return dataReturn;
+      }).catch(function (err) {
+        throw err;
+      });
+    }
+  }, {
+    key: 'bump',
+    value: function bump(type) {
+      var _this4 = this;
 
-			return new Promise(function (resolve, reject) {
-				var types = ['patch', 'minor', 'major', 'prepatch', 'preminor', 'premajor', 'prerelease'];
-				var notAType = types.indexOf(type) === -1;
-				if (type === null || type === undefined || typeof type !== 'string' || notAType) {
-					return reject(_this4._res(true, undefined, 'BUMP(): type must be specified. This is not a valid type --> \'' + type + '\''));
-				}
+      return new Promise(function (resolve, reject) {
+        var types = ['patch', 'minor', 'major', 'prepatch', 'preminor', 'premajor', 'prerelease'];
+        var notAType = types.indexOf(type) === -1;
+        if (type === null || type === undefined || typeof type !== 'string' || notAType) {
+          return reject(_this4._res(true, undefined, 'BUMP(): type must be specified. This is not a valid type --> \'' + type + '\''));
+        }
 
-				var oldVersion = '';
-				var newVersion = '';
-				var dataFiles = _this4.config.bump.files.reduce(function (acc, filePath) {
-					var fullFilePath = _path2.default.resolve(filePath);
-					var data = JSON.parse(_fsExtra2.default.readFileSync(fullFilePath, 'utf8'));
-					var old = data.version;
-					data.version = _semver2.default.inc(data.version, type);
-					oldVersion = old;
-					newVersion = data.version;
+        var oldVersion = '';
+        var newVersion = '';
+        var dataFiles = _this4.config.bump.files.reduce(function (acc, filePath) {
+          var fullFilePath = _path2.default.resolve(filePath);
+          var data = JSON.parse(_fsExtra2.default.readFileSync(fullFilePath, 'utf8'));
+          var old = data.version;
+          data.version = _semver2.default.inc(data.version, type);
+          oldVersion = old;
+          newVersion = data.version;
 
-					var dataString = JSON.stringify(data, null, '\t');
-					_fsExtra2.default.writeFileSync(fullFilePath, dataString);
-					return acc.concat(data);
-				}, []);
+          var dataString = JSON.stringify(data, null, '\t');
+          _fsExtra2.default.writeFileSync(fullFilePath, dataString);
+          return acc.concat(data);
+        }, []);
 
-				return resolve(_this4._res(undefined, dataFiles, 'Bump from ' + oldVersion + ' to ' + newVersion + ' completed.'));
-			});
-		}
-	}, {
-		key: 'upload',
-		value: function upload(language) {
-			/* istanbul ignore next */
-			var oneSky = new _i18n2.default(this.config);
-			/* istanbul ignore next */
-			return oneSky.upload(language);
-		}
-	}, {
-		key: 'download',
-		value: function download(languages) {
-			/* istanbul ignore next */
-			var oneSky = new _i18n2.default(this.config);
-			/* istanbul ignore next */
-			return oneSky.download(languages);
-		}
-	}, {
-		key: '_createWorkspace',
-		value: function _createWorkspace() {
-			try {
-				var pathWorkspace = _path2.default.resolve(this.config.workspace);
-				_fsExtra2.default.ensureDirSync(pathWorkspace);
-				_fsExtra2.default.copySync(_path2.default.resolve(this.config.sourceFolder), pathWorkspace);
-			} catch (err) {
-				throw new Error(err);
-			}
-		}
-	}, {
-		key: '_ensureWorkspace',
-		value: function _ensureWorkspace() {
-			if (_fsExtra2.default.existsSync(_path2.default.resolve(this.config.workspace))) {
-				return true;
-			}
+        return resolve(_this4._res(undefined, dataFiles, 'Bump from ' + oldVersion + ' to ' + newVersion + ' completed.'));
+      });
+    }
+  }, {
+    key: 'upload',
+    value: function upload(language) {
+      /* istanbul ignore next */
+      var oneSky = new _i18n2.default(this.config);
+      /* istanbul ignore next */
+      return oneSky.upload(language);
+    }
+  }, {
+    key: 'download',
+    value: function download(languages) {
+      /* istanbul ignore next */
+      var oneSky = new _i18n2.default(this.config);
+      /* istanbul ignore next */
+      return oneSky.download(languages);
+    }
+  }, {
+    key: '_createWorkspace',
+    value: function _createWorkspace() {
+      try {
+        var pathWorkspace = _path2.default.resolve(this.config.workspace);
+        _fsExtra2.default.ensureDirSync(pathWorkspace);
+        _fsExtra2.default.copySync(_path2.default.resolve(this.config.sourceFolder), pathWorkspace);
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+  }, {
+    key: '_ensureWorkspace',
+    value: function _ensureWorkspace() {
+      if (_fsExtra2.default.existsSync(_path2.default.resolve(this.config.workspace))) {
+        return true;
+      }
 
-			this._createWorkspace();
-		}
-	}, {
-		key: '_copyManifest',
-		value: function _copyManifest() {
-			this._ensureWorkspace();
+      this._createWorkspace();
+    }
+  }, {
+    key: '_copyManifest',
+    value: function _copyManifest() {
+      this._ensureWorkspace();
 
-			try {
-				var pathManifest = _path2.default.resolve(_path2.default.join(this.config.workspace, 'manifest.json'));
-				var pathManifestDist = _path2.default.resolve(_path2.default.join(this.config.buildFolder, 'manifest.json'));
-				var exist = _fsExtra2.default.existsSync(pathManifest);
-				if (exist) {
-					_fsExtra2.default.copySync(pathManifest, pathManifestDist);
-					return true;
-				}
+      try {
+        var pathManifest = _path2.default.resolve(_path2.default.join(this.config.workspace, 'manifest.json'));
+        var pathManifestDist = _path2.default.resolve(_path2.default.join(this.config.buildFolder, 'manifest.json'));
+        var exist = _fsExtra2.default.existsSync(pathManifest);
+        if (exist) {
+          _fsExtra2.default.copySync(pathManifest, pathManifestDist);
+          return true;
+        }
 
-				return false;
-			} catch (err) {
-				throw new Error(err);
-			}
-		}
-	}, {
-		key: '_pathErrHandler',
-		value: function _pathErrHandler(entity) {
-			var err = new Error();
-			var msg = 'Path must be a string. Received undefined';
-			err.message = msg + ' --> ' + entity;
-			return err;
-		}
-	}, {
-		key: '_res',
-		value: function _res(err, data, message) {
-			var stringified = JSON.stringify({ err: err, data: data, message: message });
-			return Object.assign({}, JSON.parse(stringified));
-		}
-	}, {
-		key: '_checkForRequired',
-		value: function _checkForRequired() {
-			if (!this.config.workspace || this.config.workspace === '') {
-				throw this._pathErrHandler('config.workspace');
-			}
+        return false;
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+  }, {
+    key: '_pathErrHandler',
+    value: function _pathErrHandler(entity) {
+      var err = new Error();
+      var msg = 'Path must be a string. Received undefined';
+      err.message = msg + ' --> ' + entity;
+      return err;
+    }
+  }, {
+    key: '_res',
+    value: function _res(err, data, message) {
+      var stringified = JSON.stringify({ err: err, data: data, message: message });
+      return Object.assign({}, JSON.parse(stringified));
+    }
+  }, {
+    key: '_checkForRequired',
+    value: function _checkForRequired() {
+      if (!this.config.workspace || this.config.workspace === '') {
+        throw this._pathErrHandler('config.workspace');
+      }
 
-			if (!this.config.sourceFolder || this.config.sourceFolder === '') {
-				throw this._pathErrHandler('config.sourceFolder');
-			}
+      if (!this.config.sourceFolder || this.config.sourceFolder === '') {
+        throw this._pathErrHandler('config.sourceFolder');
+      }
 
-			if (!this.config.buildFolder || this.config.buildFolder === '') {
-				throw this._pathErrHandler('config.buildFolder');
-			}
+      if (!this.config.buildFolder || this.config.buildFolder === '') {
+        throw this._pathErrHandler('config.buildFolder');
+      }
 
-			if (!this.config.buildJS || this.config.buildJS === '') {
-				this.config.buildJS = 'bundle.js';
-			}
+      if (!this.config.buildJS || this.config.buildJS === '') {
+        this.config.buildJS = 'bundle.js';
+      }
 
-			if (!this.config.copy) {
-				this.config.copy = [];
-			}
-		}
-	}]);
+      if (!this.config.copy) {
+        this.config.copy = [];
+      }
+    }
+  }]);
 
-	return Kubozer;
+  return Kubozer;
 }();
 
 exports.default = Kubozer;
--- dist/lib/builder.js
+++ dist/lib/builder.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -31,87 +31,87 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Builder = function () {
-	function Builder(config, webpackConfig, resFunc) {
-		_classCallCheck(this, Builder);
+  function Builder(config, webpackConfig, resFunc) {
+    _classCallCheck(this, Builder);
 
-		this.config = config;
-		this.webpackConfig = webpackConfig;
+    this.config = config;
+    this.webpackConfig = webpackConfig;
 
-		this._res = resFunc;
-	}
+    this._res = resFunc;
+  }
 
-	_createClass(Builder, [{
-		key: 'webpack',
-		value: function webpack(minify) {
-			var _this = this;
+  _createClass(Builder, [{
+    key: 'webpack',
+    value: function webpack(minify) {
+      var _this = this;
 
-			return new Promise(function (resolve, reject) {
-				_fsExtra2.default.ensureDirSync(_this.config.buildFolder);
+      return new Promise(function (resolve, reject) {
+        _fsExtra2.default.ensureDirSync(_this.config.buildFolder);
 
-				if (minify) {
-					var uglify = new _webpack3.default.optimize.UglifyJsPlugin({
-						comments: false,
-						compress: {
-							warnings: false,
-							/* eslint-disable camelcase */
-							drop_console: _this.config.stripConsole
-						},
-						sourceMap: true
-					});
+        if (minify) {
+          var uglify = new _webpack3.default.optimize.UglifyJsPlugin({
+            comments: false,
+            compress: {
+              warnings: false,
+              /* eslint-disable camelcase */
+              drop_console: _this.config.stripConsole
+            },
+            sourceMap: true
+          });
 
-					_this.webpackConfig.plugins = _this.webpackConfig.plugins ? _this.webpackConfig.plugins.concat(uglify) : [uglify];
-					_this.webpackConfig.plugins = _this.webpackConfig.plugins.concat(new _webpackNotifier2.default({
-						title: 'Kubozer - Webpack',
-						contentImage: _path2.default.join(__dirname, './../../', 'Kubozer_Sign@2x.png')
-					}));
-				}
+          _this.webpackConfig.plugins = _this.webpackConfig.plugins ? _this.webpackConfig.plugins.concat(uglify) : [uglify];
+          _this.webpackConfig.plugins = _this.webpackConfig.plugins.concat(new _webpackNotifier2.default({
+            title: 'Kubozer - Webpack',
+            contentImage: _path2.default.join(__dirname, './../../', 'Kubozer_Sign@2x.png')
+          }));
+        }
 
-				try {
-					var compiler = (0, _webpack3.default)(_this.webpackConfig);
-					compiler.run(function (err) {
-						if (err) {
-							return reject(_this._res(true, undefined, err));
-						}
-						return resolve(_this._res(undefined, [{ completed: true }], 'Webpack compilation completed'));
-					});
-				} catch (err) {
-					return reject(_this._res(err.name, undefined, err.message));
-				}
-			});
-		}
-	}, {
-		key: 'vulcanize',
-		value: function vulcanize() {
-			var _this2 = this;
+        try {
+          var compiler = (0, _webpack3.default)(_this.webpackConfig);
+          compiler.run(function (err) {
+            if (err) {
+              return reject(_this._res(true, undefined, err));
+            }
+            return resolve(_this._res(undefined, [{ completed: true }], 'Webpack compilation completed'));
+          });
+        } catch (err) {
+          return reject(_this._res(err.name, undefined, err.message));
+        }
+      });
+    }
+  }, {
+    key: 'vulcanize',
+    value: function vulcanize() {
+      var _this2 = this;
 
-			return new Promise(function (resolve, reject) {
-				if (_this2.config.vulcanize === undefined) {
-					reject(_this2._res(true, undefined, 'Vulcanize configuration is not present. ---> config.vulcanize === undefined'));
-				}
+      return new Promise(function (resolve, reject) {
+        if (_this2.config.vulcanize === undefined) {
+          reject(_this2._res(true, undefined, 'Vulcanize configuration is not present. ---> config.vulcanize === undefined'));
+        }
 
-				var vulcan = new _vulcanize2.default(_this2.config.vulcanize.conf);
+        var vulcan = new _vulcanize2.default(_this2.config.vulcanize.conf);
 
-				var workspaceIndex = _path2.default.join(_path2.default.resolve(_this2.config.workspace), _this2.config.vulcanize.srcTarget);
-				var buildIndex = _path2.default.join(_path2.default.resolve(_this2.config.buildFolder), _this2.config.vulcanize.buildTarget);
+        var workspaceIndex = _path2.default.join(_path2.default.resolve(_this2.config.workspace), _this2.config.vulcanize.srcTarget);
+        var buildIndex = _path2.default.join(_path2.default.resolve(_this2.config.buildFolder), _this2.config.vulcanize.buildTarget);
 
-				vulcan.process(workspaceIndex, function (err, inlinedHTML) {
-					if (err) {
-						var msg = err.message + ' | Did you checked the "excludes" property of "vulcanize" configuration?';
-						return reject(_this2._res(true, undefined, err.message.search('no such file') > -1 ? msg : err.message));
-					}
-					_fsExtra2.default.ensureFileSync(buildIndex);
-					_fsExtra2.default.writeFile(buildIndex, inlinedHTML, function (err) {
-						if (err) {
-							return reject(err);
-						}
-						return resolve(_this2._res(undefined, buildIndex, 'Vulcanize completed.'));
-					});
-				});
-			});
-		}
-	}]);
+        vulcan.process(workspaceIndex, function (err, inlinedHTML) {
+          if (err) {
+            var msg = err.message + ' | Did you checked the "excludes" property of "vulcanize" configuration?';
+            return reject(_this2._res(true, undefined, err.message.search('no such file') > -1 ? msg : err.message));
+          }
+          _fsExtra2.default.ensureFileSync(buildIndex);
+          _fsExtra2.default.writeFile(buildIndex, inlinedHTML, function (err) {
+            if (err) {
+              return reject(err);
+            }
+            return resolve(_this2._res(undefined, buildIndex, 'Vulcanize completed.'));
+          });
+        });
+      });
+    }
+  }]);
 
-	return Builder;
+  return Builder;
 }();
 
 exports.default = Builder;
