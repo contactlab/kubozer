ea587f4c5675b632626594c5a1d6cd6751d65620
StefanoMagrassi <stefano.magrassi@gmail.com>
2017-05-12 17:22:59 +0200

    success/error methods for result lib

--- dist/lib/result.js
+++ dist/lib/result.js
@@ -12,4 +12,13 @@ var result = function result(err, data, message) {
   return Object.assign({}, { err: err, data: data, message: message });
 };
 
+var success = exports.success = function success(message, data) {
+  return result(undefined, data, message);
+};
+
+var error = exports.error = function error(message) {
+  var type = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
+  return result(type, undefined, message);
+};
+
 exports.default = result;
--- src/lib/result.js
+++ src/lib/result.js
@@ -5,4 +5,8 @@
 
 const result = (err, data, message) => Object.assign({}, {err, data, message});
 
+export const success = (message, data) => result(undefined, data, message);
+
+export const error = (message, type = true) => result(type, undefined, message);
+
 export default result;
