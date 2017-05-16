d870e0def9a7cdcfe6e3b47ec6a9c2fa6366a123
StefanoMagrassi <stefano.magrassi@gmail.com>
2017-05-16 15:52:43 +0200

    use helpers in replace.test

--- test/replace.test.js
+++ test/replace.test.js
@@ -1,30 +1,26 @@
 /* eslint "key-spacing": ["error", {"align": "colon"}] */
 
-import path from 'path';
 import fs   from 'fs-extra';
 import test from 'ava';
 
-import
-  replaceWithHashed,
-  {ERROR_CONF_MSG, ERROR_REPLACEMENT_MSG}
-from '../dist/lib/replace';
+import replaceWithHashed, {ERROR_CONF_MSG, ERROR_REPLACEMENT_MSG} from '../dist/lib/replace';
+import helpers from './helpers/integration';
 
-const DIST_DIR    = 'dist_replace';
-const INTEGRATION = path.resolve(__dirname, 'integration');
-const TPL         = path.resolve(INTEGRATION, 'template');
-const DIST        = path.resolve(INTEGRATION, DIST_DIR);
-const CONFIG      = {
-  buildFolder : `./test/integration/${DIST_DIR}`,
+const NAME = 'replace';
+
+const CONFIG = {
+  buildFolder : `./test/integration/${helpers.dist(NAME)}`,
   assetsFolder: 'assets',
   buildCssFile: 'css/style.css'
 };
+
 const REPLACEMENT = {
   from: ['bundle.js', 'assets/css/style.css'],
   to  : ['bundle-asdasd.js', 'assets/css/style-asdasd.css']
 };
 
-test.cb.before(t => {
-  fs.copy(TPL, DIST, t.end);
+test.before(async () => {
+  await helpers.distFromTpl(NAME);
 });
 
 test('replaceWithHashed', t => {
@@ -33,12 +29,13 @@ test('replaceWithHashed', t => {
 
 test('replaceWithHashed(config)(replacement)', async t => {
   const replaceMap = await replaceWithHashed(CONFIG)(REPLACEMENT);
-  const result     = fs.readFileSync(replaceMap[0], 'utf8');
+  const result     = await fs.readFile(replaceMap[0], 'utf8');
+
   const jsSnippet  = '<script src="bundle-asdasd.js"></script>';
   const cssSnippet = '<link rel="stylesheet" href="assets/css/style-asdasd.css" />';
 
-  t.true(result.indexOf(jsSnippet) > -1, 'should replace bundle file');
-  t.true(result.indexOf(cssSnippet) > -1, 'should replace styles file');
+  t.true(result.includes(jsSnippet), 'should replace bundle file');
+  t.true(result.includes(cssSnippet), 'should replace styles file');
 });
 
 test('replaceWithHashed(undefined)(replacement)', async t => {
@@ -53,6 +50,6 @@ test('replaceWithHashed(config, undefined)', async t => {
   t.is(err.message, ERROR_REPLACEMENT_MSG, 'should reject with an error');
 });
 
-test.cb.after(t => {
-  fs.remove(DIST, t.end);
+test.after.always(async () => {
+  await helpers.clean(NAME);
 });
