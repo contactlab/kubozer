6cc8fb6b11affa819544359a1447dc65c1cc58bc
StefanoMagrassi <stefano.magrassi@gmail.com>
2017-05-18 09:53:20 +0200

    use tmp-dir helper in hashed.test

--- test/hashed.test.js
+++ test/hashed.test.js
@@ -1,28 +1,45 @@
 /* eslint "key-spacing": ["error", {"align": "colon"}] */
 
 import path from 'path';
+import fs   from 'fs-extra';
 import test from 'ava';
 
 import hashed, {ERROR_MSG} from '../dist/lib/hashed';
-import helpers             from './helpers/integration';
+// import helpers             from './helpers/integration';
+import tmpDir             from './helpers/tmp-dir';
 
-const NAME = 'hashed';
+// const NAME = 'hashed';
 
-const CONFIG = {
-  buildFolder : `./test/integration/${helpers.dist(NAME)}`,
+// const CONFIG = {
+//   buildFolder : `./test/integration/${helpers.dist(NAME)}`,
+//   assetsFolder: 'assets',
+//   buildCssFile: 'css/style.css'
+// };
+const _config = dir => ({
+  buildFolder : path.join(dir, 'dist'),
   assetsFolder: 'assets',
   buildCssFile: 'css/style.css'
-};
+});
 
-const WEBPACKCONFIG = {
+// const WEBPACKCONFIG = {
+//   output: {
+//     path    : `./test/integration/${helpers.dist(NAME)}`,
+//     filename: 'bundle.js'
+//   }
+// };
+const _webpackConfig = dir => ({
   output: {
-    path    : `./test/integration/${helpers.dist(NAME)}`,
+    path    : path.join(dir, 'dist'),
     filename: 'bundle.js'
   }
-};
+});
 
-test.before(async () => {
-  await helpers.distFromTpl(NAME);
+// test.before(async () => {
+//   await helpers.distFromTpl(NAME);
+// });
+test.beforeEach(async t => {
+  // eslint-disable-next-line ava/use-t-well
+  t.context.tmpDir = await tmpDir(path.join(__dirname, 'integration/dist'), t.title);
 });
 
 test('hashed', t => {
@@ -30,10 +47,12 @@ test('hashed', t => {
 });
 
 test('hashed(config, webpackconfig)', async t => {
-  const result = await hashed(CONFIG, WEBPACKCONFIG);
+  const config  = _config(t.context.tmpDir);
+  const webpack = _webpackConfig(t.context.tmpDir);
+  const result  = await hashed(config, webpack);
 
-  const bundleFile = path.join(WEBPACKCONFIG.output.path, WEBPACKCONFIG.output.filename);
-  const styleFile  = path.join(CONFIG.buildFolder, CONFIG.assetsFolder, CONFIG.buildCssFile);
+  const bundleFile = path.relative('.', path.join(webpack.output.path, webpack.output.filename));
+  const styleFile  = path.relative('.', path.join(config.buildFolder, config.assetsFolder, config.buildCssFile));
 
   const bundleFileBase = path.join(path.dirname(bundleFile), path.basename(bundleFile, '.js'));
   const styleFileBase  = path.join(path.dirname(styleFile), path.basename(styleFile, '.css'));
@@ -45,17 +64,24 @@ test('hashed(config, webpackconfig)', async t => {
 });
 
 test('hashed(undefined, webpackconfig)', async t => {
-  const err = await t.throws(hashed(undefined, WEBPACKCONFIG));
+  // const config  = _config(t.context.tmpDir);
+  const webpack = _webpackConfig(t.context.tmpDir);
+  const err = await t.throws(hashed(undefined, webpack));
 
   t.is(err.message, ERROR_MSG, 'should reject with an error');
 });
 
 test('hashed(config, undefined)', async t => {
-  const err = await t.throws(hashed(CONFIG, undefined));
+  const config  = _config(t.context.tmpDir);
+  // const webpack = _webpackConfig(t.context.tmpDir);
+  const err = await t.throws(hashed(config, undefined));
 
   t.is(err.message, ERROR_MSG, 'should reject with an error');
 });
 
-test.after.always(async () => {
-  await helpers.clean(NAME);
+// test.after.always(async () => {
+//   await helpers.clean(NAME);
+// });
+test.afterEach.always(async t => {
+  await fs.remove(t.context.tmpDir);
 });
