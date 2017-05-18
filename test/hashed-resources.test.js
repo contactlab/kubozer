d2e127b47cef3eae3348d07f0c074227af6d696d
StefanoMagrassi <stefano.magrassi@gmail.com>
2017-05-18 10:18:37 +0200

    use tmp-dir helper in hashed-resources.test

--- test/hashed-resources.test.js
+++ test/hashed-resources.test.js
@@ -5,25 +5,28 @@ import fs   from 'fs-extra';
 import test from 'ava';
 
 import hashedResources, {SUCCESS_MSG, ERROR_MSG} from '../dist/lib/hashed-resources';
-import helpers                                   from './helpers/integration';
+import tmpDir                                    from './helpers/tmp-dir';
 
-const NAME = 'hashed_resources';
-
-const CONFIG = {
-  buildFolder : `./test/integration/${helpers.dist(NAME)}`,
+const _config = dir => ({
+  buildFolder : path.join(dir, 'dist'),
   assetsFolder: 'assets',
   buildCssFile: 'css/style.css'
-};
+});
 
-const WEBPACKCONFIG = {
+const _webpackConfig = dir => ({
   output: {
-    path    : `./test/integration/${helpers.dist(NAME)}`,
+    path    : path.join(dir, 'dist'),
     filename: 'bundle.js'
   }
-};
+});
 
-test.before(async () => {
-  await helpers.distFromTpl(NAME);
+test.beforeEach(async t => {
+  // eslint-disable-next-line ava/use-t-well
+  const workDir = await tmpDir(path.join(__dirname, 'integration/dist'), t.title);
+
+  t.context.tmpDir = workDir;
+  t.context.config = _config(workDir);
+  t.context.webpack = _webpackConfig(workDir);
 });
 
 test('hashedResources', t => {
@@ -31,11 +34,13 @@ test('hashedResources', t => {
 });
 
 test('hashedResources(config, webpackconfig)', async t => {
-  const {output}                                  = WEBPACKCONFIG;
-  const {buildFolder, assetsFolder, buildCssFile} = CONFIG;
+  const {config, webpack, tmpDir}                 = t.context;
+  const {output}                                  = webpack;
+  const {buildFolder, assetsFolder, buildCssFile} = config;
+  const distDir                                   = path.join(tmpDir, 'dist');
 
-  const bundleFile = path.relative(helpers.distDir(NAME), path.resolve(output.path, output.filename));
-  const stylesFile = path.relative(helpers.distDir(NAME), path.resolve(buildFolder, assetsFolder, buildCssFile));
+  const bundleFile = path.relative(distDir, path.resolve(output.path, output.filename));
+  const stylesFile = path.relative(distDir, path.resolve(buildFolder, assetsFolder, buildCssFile));
 
   const bundleFileBase = path.join(path.dirname(bundleFile), path.basename(bundleFile, '.js'));
   const stylesFileBase = path.join(path.dirname(stylesFile), path.basename(stylesFile, '.css'));
@@ -43,30 +48,32 @@ test('hashedResources(config, webpackconfig)', async t => {
   const bundleMatch = ` src="${bundleFileBase}-`;
   const stylesMatch = ` href="${stylesFileBase}-`;
 
-  const result = await hashedResources(CONFIG, WEBPACKCONFIG);
+  const result = await hashedResources(config, webpack);
 
   t.is(result.message, SUCCESS_MSG, 'should exit with success message');
 
-  const fileContent = await fs.readFile(path.resolve(helpers.distDir(NAME), 'index.html'), 'utf8');
+  const fileContent = await fs.readFile(path.join(distDir, 'index.html'), 'utf8');
 
   t.true(fileContent.indexOf(bundleMatch) > -1, 'index.html should contain the hashed version of bundle file');
   t.true(fileContent.indexOf(stylesMatch) > -1, 'index.html should contain the hashed version of styles file');
 });
 
 test('hashedResources(undefined, webpackconfig)', async t => {
-  const err = await t.throws(hashedResources(undefined, WEBPACKCONFIG));
+  const {webpack} = t.context;
+  const err       = await t.throws(hashedResources(undefined, webpack));
 
   t.true(err.err, 'should return an error');
   t.is(err.message, ERROR_MSG, 'should reject with an error message');
 });
 
 test('hashedResources(config, undefined)', async t => {
-  const err = await t.throws(hashedResources(CONFIG, undefined));
+  const {config} = t.context;
+  const err      = await t.throws(hashedResources(config, undefined));
 
   t.true(err.err, 'should return an error');
   t.is(err.message, ERROR_MSG, 'should reject with an error message');
 });
 
-test.after.always(async () => {
-  await helpers.clean(NAME);
+test.afterEach.always(async t => {
+  await fs.remove(t.context.tmpDir);
 });
