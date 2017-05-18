6d83a4f2ecb2840ab3d3cb89481b930ddee41a4d
StefanoMagrassi <stefano.magrassi@gmail.com>
2017-05-18 14:32:35 +0200

    cleaned up test/i18n.js

--- test/i18n.js
+++ test/i18n.js
@@ -1,144 +1,134 @@
-import test from 'ava';
-import Fn from './../dist/lib/i18n';
+/* eslint "key-spacing": ["error", {"align": "colon"}] */
 
-test('get correct filePath for language file', async t => {
+import test from 'ava';
+import I18n from '../dist/lib/i18n';
+
+test('get correct filePath for language file', t => {
   const config = {
     i18n: {
-      secret: 'aaa',
-      apiKey: 'aaaa',
-      projectId: 'aaaaa',
+      secret         : 'aaa',
+      apiKey         : 'aaaa',
+      projectId      : 'aaaaa',
       defaultLanguage: 'en',
-      format: 'HIERARCHICAL_JSON',
-      projectID: 94359,
-      languagesPath: './app/bundles'
+      format         : 'HIERARCHICAL_JSON',
+      projectID      : 94359,
+      languagesPath  : './app/bundles'
     }
   };
-  const fn = new Fn(config);
-  const filePath = fn.getFilePath(config.i18n.languagesPath, 'en');
+
+  const i18n     = new I18n(config);
+  const filePath = i18n.getFilePath(config.i18n.languagesPath, 'en');
+
   t.is(filePath, 'app/bundles/en.json');
 });
 
-test('get correct fileName for language file', async t => {
+test('get correct fileName for language file', t => {
   const config = {
     i18n: {
-      secret: 'aaa',
-      apiKey: 'aaaa',
-      projectId: 'aaaaa',
+      secret         : 'aaa',
+      apiKey         : 'aaaa',
+      projectId      : 'aaaaa',
       defaultLanguage: 'en',
-      format: 'HIERARCHICAL_JSON',
-      projectID: 94359,
-      languagesPath: './app/bundles'
+      format         : 'HIERARCHICAL_JSON',
+      projectID      : 94359,
+      languagesPath  : './app/bundles'
     }
   };
-  const fn = new Fn(config);
-  const fileName = fn.getFileName('en');
+
+  const i18n     = new I18n(config);
+  const fileName = i18n.getFileName('en');
+
   t.is(fileName, 'en.json');
 });
 
 test('check required i18n key in config', t => {
-  const config = {};
-
-  const err = t.throws(() => {
-    new Fn(config)
-  }, 'In order to use OneSky integration, you need i18n configuration');
+  t.throws(() => new I18n({}), 'In order to use OneSky integration, you need i18n configuration');
 });
 
 test('check required secret key in config', t => {
   const config = {
     i18n: {
-      apiKey: 'aaaa',
-      projectId: 'aaaaa',
+      apiKey         : 'aaaa',
+      projectId      : 'aaaaa',
       defaultLanguage: 'en',
-      format: 'HIERARCHICAL_JSON',
-      projectID: 94359,
-      languagesPath: './app/bundles'
+      format         : 'HIERARCHICAL_JSON',
+      projectID      : 94359,
+      languagesPath  : './app/bundles'
     }
   };
 
-  const err = t.throws(() => {
-    new Fn(config)
-  }, 'Path must be a string. Received undefined --> config.i18n.secret');
+  t.throws(() => new I18n(config), 'Path must be a string. Received undefined --> config.i18n.secret');
 });
 
 test('check required apiKey key in config', t => {
   const config = {
     i18n: {
-      secret: 'aaa',
-      projectId: 'aaaaa',
+      secret         : 'aaa',
+      projectId      : 'aaaaa',
       defaultLanguage: 'en',
-      format: 'HIERARCHICAL_JSON',
-      projectID: 94359,
-      languagesPath: './app/bundles'
+      format         : 'HIERARCHICAL_JSON',
+      projectID      : 94359,
+      languagesPath  : './app/bundles'
     }
   };
 
-  const err = t.throws(() => {
-    new Fn(config)
-  }, 'Path must be a string. Received undefined --> config.i18n.apiKey');
+  t.throws(() => new I18n(config), 'Path must be a string. Received undefined --> config.i18n.apiKey');
 });
 
 test('check required projectId key in config', t => {
   const config = {
     i18n: {
-      secret: 'aaa',
-      apiKey: 'aaaa',
+      secret         : 'aaa',
+      apiKey         : 'aaaa',
       defaultLanguage: 'en',
-      format: 'HIERARCHICAL_JSON',
-      languagesPath: './app/bundles'
+      format         : 'HIERARCHICAL_JSON',
+      languagesPath  : './app/bundles'
     }
   };
 
-  const err = t.throws(() => {
-    new Fn(config)
-  }, 'Path must be a string. Received undefined --> config.i18n.projectId');
+  t.throws(() => new I18n(config), 'Path must be a string. Received undefined --> config.i18n.projectId');
 });
 
 test('check required defaultLanguage key in config', t => {
   const config = {
     i18n: {
-      secret: 'aaa',
-      apiKey: 'aaaa',
-      projectId: 'aaaaa',
-      format: 'HIERARCHICAL_JSON',
-      projectID: 94359,
+      secret       : 'aaa',
+      apiKey       : 'aaaa',
+      projectId    : 'aaaaa',
+      format       : 'HIERARCHICAL_JSON',
+      projectID    : 94359,
       languagesPath: './app/bundles'
     }
   };
 
-  const err = t.throws(() => {
-    new Fn(config)
-  }, 'Path must be a string. Received undefined --> config.i18n.defaultLanguage');
+  t.throws(() => new I18n(config), 'Path must be a string. Received undefined --> config.i18n.defaultLanguage');
 });
 
 test('check required format key in config', t => {
   const config = {
     i18n: {
-      secret: 'aaa',
-      apiKey: 'aaaa',
-      projectId: 'aaaaa',
+      secret         : 'aaa',
+      apiKey         : 'aaaa',
+      projectId      : 'aaaaa',
       defaultLanguage: 'en',
-      projectID: 94359,
-      languagesPath: './app/bundles'
+      projectID      : 94359,
+      languagesPath  : './app/bundles'
     }
   };
 
-  const err = t.throws(() => {
-    new Fn(config)
-  }, 'Path must be a string. Received undefined --> config.i18n.format');
+  t.throws(() => new I18n(config), 'Path must be a string. Received undefined --> config.i18n.format');
 });
 
 test('check required languagesPath key in config', t => {
   const config = {
     i18n: {
-      secret: 'aaa',
-      apiKey: 'aaaa',
-      projectId: 'aaaaa',
+      secret         : 'aaa',
+      apiKey         : 'aaaa',
+      projectId      : 'aaaaa',
       defaultLanguage: 'en',
-      format: 'HIERARCHICAL_JSON',
+      format         : 'HIERARCHICAL_JSON'
     }
   };
 
-  const err = t.throws(() => {
-    new Fn(config)
-  }, 'Path must be a string. Received undefined --> config.i18n.languagesPath');
+  t.throws(() => new I18n(config), 'Path must be a string. Received undefined --> config.i18n.languagesPath');
 });
