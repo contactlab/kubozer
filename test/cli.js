bf8490d05bf6a0282c50ebf59c00cd42ed890360
StefanoMagrassi <stefano.magrassi@gmail.com>
2017-05-18 17:31:25 +0200

    test/cli: async and isolated

--- test/cli.js
+++ test/cli.js
@@ -1,272 +1,216 @@
-import fs from 'fs-extra';
-import test from 'ava';
-import execa from 'execa';
-import figures from 'figures';
-import chalk from 'chalk';
+/* eslint "key-spacing": ["error", {"align": "colon"}] */
 
-test.beforeEach(t => {
-    const copied = fs.copySync(__dirname + '/../kubozer.conf.js', __dirname + '/../kubozer.conf.js.backup');
+import path    from 'path';
+import fs      from 'fs-extra';
+import test    from 'ava';
+import execa   from 'execa';
+import figures from 'figures';
+import chalk   from 'chalk';
+
+import tmpDir from './helpers/tmp-dir';
+import hconf  from './helpers/config';
+
+const CLI = path.join(__dirname, '../dist/cli.js');
+const DIR = 'src';
+
+// --- Utilities
+const red         = chalk.red;
+const severe      = red.bold.underline;
+const warn        = chalk.underline.yellow;
+const green       = chalk.green;
+const merge       = (...args) => Object.assign.apply(null, [{}, ...args]);
+const intoParent  = (dir, file) => fs.move(path.join(dir, DIR, file), path.join(dir, file), {overwrite: true});
+const kubozerConf = (dir, config) =>
+  fs.writeFile(path.join(dir, 'kubozer.conf.js'), `module.exports = ${JSON.stringify(config)};`);
+// --- /Utilities
+
+test.beforeEach(async t => {
+  // eslint-disable-next-line ava/use-t-well
+  const workDir = await tmpDir(path.join(__dirname, 'integration/cli'), t.title);
+
+  await fs.move(path.join(workDir, 'cli'), path.join(workDir, DIR));
+  await intoParent(workDir, 'webpack.config.js');
+
+  t.context.tmpDir = workDir;
 });
 
 test('throws when a file is not found during BUMP', async t => {
-  const wrote = fs.writeFileSync(__dirname + '/../kubozer.conf.js', `module.exports = {
-  workspace: './test/workspace',
-  sourceFolder: './test/src-test',
-  buildFolder: './test/build',
-  assetsFolder: 'assets',
-  sourceCssFiles: ['/test.css'],
-  buildCssFile: 'style.min.css',
-  manifest: true,
-  bump: {
-    files: [
-      './test/src-test/package.json',
-      './test/src-test/manifest.json',
-      './test/src-test/bower.json'
-    ]
-  },
-  // Copy object NOT PRESENT
-  vulcanize: {
-    srcTarget: 'index.html',
-    buildTarget: 'index.html',
-    conf: {
-      stripComments: true,
-      inlineScripts: true,
-      inlineStyles: true,
-      excludes: ['bundle.js']
+  const {tmpDir} = t.context;
+
+  const extra  = path.join(tmpDir, DIR, 'bower.json');
+  const config = merge(hconf(tmpDir, ['FOLDERS', 'CSS', 'MANIFEST', 'VULCANIZE_NO_BUNDLE_STD']), {
+    bump: {
+      files: [
+        path.join(tmpDir, DIR, 'package.json'),
+        path.join(tmpDir, DIR, 'manifest.json'),
+        extra
+      ]
     }
-  }
-};`);
-  const msg = (await execa(__dirname + '/../dist/cli.js', ['--bump', 'major'])).stderr;
-  let expectedOutput = chalk.red(figures.cross) + ' Bumped version.';
-  expectedOutput += chalk.bold.underline.red(`\n⚠️ ERROR: ENOENT: no such file or directory, open '${__dirname}/src-test/bower.json'`);
-  t.is(msg, expectedOutput);
-})
+  });
+
+  await kubozerConf(tmpDir, config);
+
+  const actual   = await execa.stderr(CLI, ['--bump', 'major'], {cwd: tmpDir});
+  const expected = (red(figures.cross) + ' Bumped version.')
+                    .concat(severe(`\n⚠️ ERROR: ENOENT: no such file or directory, open '${extra}'`));
+
+  t.is(actual, expected);
+});
 
 test('throws param is not passed to BUMP', async t => {
-  const msg = (await execa(__dirname + '/../dist/cli.js', ['--bump'])).stderr;
-  let expectedOutput = chalk.red(figures.cross) + ' Bumped version.';
-  expectedOutput += chalk.bold.underline.red(`\n⚠️ ERROR: BUMP(): type must be specified. This is not a valid type --> 'true'`);
-  t.is(msg, expectedOutput);
-})
+  const {tmpDir} = t.context;
+
+  await intoParent(tmpDir, 'kubozer.conf.js');
+
+  const actual   = await execa.stderr(CLI, ['--bump'], {cwd: tmpDir});
+  const expected = (red(figures.cross) + ' Bumped version.')
+                    .concat(severe(`\n⚠️ ERROR: BUMP(): type must be specified. This is not a valid type --> 'true'`));
+
+  t.is(actual, expected);
+});
 
 test('throws on constructor initialization', async t => {
-    const wrote = fs.writeFileSync(__dirname + '/../kubozer.conf.js', `module.exports = {
-  workspace: './test/workspace',
-  buildFolder: './test/build'
-};`);
-  const msg = (await execa(__dirname + '/../dist/cli.js', ['--bump'])).stderr;
-  let expectedOutput = chalk.bold.underline.red(`\n⚠️ ERROR: Path must be a string. Received undefined --> config.sourceFolder`);
-  t.is(msg, expectedOutput);
-})
+  const {tmpDir} = t.context;
+  const config   = {
+    workspace  : path.join(tmpDir, 'workspace'),
+    buildFolder: path.join(tmpDir, 'build')
+  };
+
+  await kubozerConf(tmpDir, config);
+
+  const actual   = await execa.stderr(CLI, ['--bump'], {cwd: tmpDir});
+  const expected = severe(`\n⚠️ ERROR: Path must be a string. Received undefined --> config.sourceFolder`);
+
+  t.is(actual, expected);
+});
 
 test('throw if `copy` object is not present', async t => {
-  const wrote = fs.writeFileSync(__dirname + '/../kubozer.conf.js', `module.exports = {
-  workspace: './test/workspace',
-  sourceFolder: './test/src-test',
-  buildFolder: './test/build',
-  buildJS: 'bundle.js',
-  assetsFolder: 'assets',
-  sourceCssFiles: ['/test.css'],
-  buildCssFile: 'style.min.css',
-  manifest: true,
-  bump: {
-    files: [
-      './test/src-test/package.json',
-      './test/src-test/manifest.json'
-    ]
-  },
-  // Copy object NOT PRESENT
-  vulcanize: {
-    srcTarget: 'index.html',
-    buildTarget: 'index.html',
-    conf: {
-      stripComments: true,
-      inlineScripts: true,
-      inlineStyles: true,
-      excludes: ['bundle.js']
-    }
-  }
-};`);
+  const {tmpDir} = t.context;
+  const config   = hconf(tmpDir, ['FOLDERS', 'CSS', 'MANIFEST', 'BUMP', 'VULCANIZE_NO_BUNDLE_STD']);
 
-  const msg = (await execa.shell('NODE_ENV=production ' +__dirname + '/../dist/cli.js --build')).stderr;
+  await kubozerConf(tmpDir, config);
 
-  let expectedOutput = chalk.red(figures.cross) + ' COPY: Files copied correctly.';
-  expectedOutput += chalk.bold.underline.red('\n⚠️ ERROR: copy() method was called but \"copy\" property is empty or undefined.');
+  const actual   = await execa.stderr(CLI, ['--build'], {cwd: tmpDir, env: {NODE_ENV: 'production'}});
+  const expected = (red(figures.cross) + ' COPY: Files copied correctly.')
+                  // eslint-disable-next-line no-useless-escape
+                  .concat(severe('\n⚠️ ERROR: copy() method was called but \"copy\" property is empty or undefined.'));
 
-  t.is(msg, expectedOutput)
-})
+  t.is(actual, expected);
+});
 
 test('throw if not found elment to `copy`', async t => {
-  const wrote = fs.writeFileSync(__dirname + '/../kubozer.conf.js', `module.exports = {
-  workspace: './test/workspace',
-  sourceFolder: './test/src-test',
-  buildFolder: './test/build',
-  assetsFolder: 'assets',
-  sourceCssFiles: ['/test.css'],
-  buildCssFile: 'style.min.css',
-  manifest: true,
-  bump: {
-    files: [
-      './test/src-test/package.json',
-      './test/src-test/manifest.json'
+  const {tmpDir} = t.context;
+  const config   = merge(hconf(tmpDir, ['FOLDERS', 'CSS', 'MANIFEST', 'BUMP', 'VULCANIZE_NO_BUNDLE_STD', 'REPLACE']), {
+    copy: [
+      {
+        baseFolder: 'assets',
+        items     : ['imgs-asdasdasdasdasdasdasd']
+      }, {
+        baseFolder: 'bundles',
+        items     : []
+      }
     ]
-  },
-  copy: [
-    {
-      baseFolder: 'assets',
-      items: [
-        'imgs-asdasdasdasdasdasdasd'
-      ]
-    }, {
-      baseFolder: 'bundles',
-      items: []
-    }
-  ],
-  vulcanize: {
-    srcTarget: 'index.html',
-    buildTarget: 'index.html',
-    conf: {
-      stripComments: true,
-      inlineScripts: true,
-      inlineStyles: true,
-      excludes: ['bundle.js']
-    }
-  },
-  replace: {
-    css: {
-      files: 'index.html',
-      commentRegex: ['<!--styles!--->((.|)*)<!--styles!--->'],
-      with: ['assets/style.min.css']
-    },
-    js: {
-      files: 'index.html',
-      commentRegex: ['<!--js!--->((.|)*)<!--js!--->'],
-      with: ['bundle.js']
-    }
-  }
-};`);
+  });
 
-  const msg = (await execa.shell(__dirname + '/../dist/cli.js --build')).stderr;
-  let expectedOutput = chalk.red(figures.cross) + ' COPY: Files copied correctly.';
-  expectedOutput += chalk.bold.underline.red(`\n⚠️ ERROR: ENOENT: no such file or directory, stat '${__dirname}/workspace/assets/imgs-asdasdasdasdasdasdasd'`);
-  t.is(msg, expectedOutput)
-})
+  await kubozerConf(tmpDir, config);
+
+  const img      = path.join(config.workspace, config.assetsFolder, 'imgs-asdasdasdasdasdasdasd');
+  const actual   = await execa.stderr(CLI, ['--build'], {cwd: tmpDir});
+  const expected = (red(figures.cross) + ' COPY: Files copied correctly.')
+                    .concat(severe(`\n⚠️ ERROR: ENOENT: no such file or directory, stat '${img}'`));
+
+  t.is(actual, expected);
+});
 
 test('warn when webpack output.path is NOT the same of kubozer buildFolder', async t => {
-  const wrote = fs.writeFileSync(__dirname + '/../kubozer.conf.js', `module.exports = {
-  workspace: './test/workspace',
-  sourceFolder: './test/src-test',
-  buildFolder: './test/build/another',
-  assetsFolder: 'assets',
-  sourceCssFiles: ['/test.css'],
-  buildCssFile: 'style.min.css',
-  manifest: true,
-  bump: {
-    files: [
-      './test/src-test/package.json',
-      './test/src-test/manifest.json'
-    ]
-  },
-  copy: [
-    {
-      baseFolder: 'assets',
-      items: [
-        'imgs-others'
-      ]
-    }, {
-      baseFolder: 'bundles',
-      items: [
-        ''
-      ]
+  const {tmpDir} = t.context;
+  const config   = merge(hconf(tmpDir, ['FOLDERS', 'CSS', 'MANIFEST', 'BUMP', 'COPY', 'VULCANIZE_NO_BUNDLE_STD']), {
+    buildFolder: path.join(tmpDir, 'build/another'),
+    replace    : {
+      css: {
+        files       : 'index.html',
+        commentRegex: ['<!--styles!--->((.|)*)<!--styles!--->'],
+        with        : ['assets/style.min.css']
+      },
+      js: {
+        files       : 'index.html',
+        commentRegex: ['<!--js!--->((.|)*)<!--js!--->'],
+        with        : ['bundle.js']
+      }
     }
-  ],
-  vulcanize: {
-    srcTarget: 'index.html',
-    buildTarget: 'index.html',
-    conf: {
-      stripComments: true,
-      inlineScripts: true,
-      inlineStyles: true,
-      excludes: ['bundle.js']
-    }
-  },
-  replace: {
-    css: {
-      files: 'index.html',
-      commentRegex: ['<!--styles!--->((.|)*)<!--styles!--->'],
-      with: ['assets/style.min.css']
-    },
-    js: {
-      files: 'index.html',
-      commentRegex: ['<!--js!--->((.|)*)<!--js!--->'],
-      with: ['bundle.js']
-    }
-  }
-};`);
+  });
 
-  const msg = (await execa.shell(__dirname + '/../dist/cli.js --build'));
-  let expectedOutputErr = chalk.green(figures.tick) + ' REPLACE: HTML content replaced correctly.\n';
-    expectedOutputErr += chalk.green(figures.tick) + ' BUILD: Build JS and HTML completed correctly.\n';
-    expectedOutputErr += chalk.green(figures.tick) + ' >> Building...\n';
-    expectedOutputErr += chalk.green(figures.tick) + ' Everything works with charme 🚀';
-  let expectedOutput = '\n> Started STAGING build'
-  expectedOutput += chalk.underline.yellow('\n⚠️ WARNING: the "buildFolder" and the "webpackConfig.output.path" are not the same.')
-  //  t.is(msg.stderr, expectedOutputErr);
-   t.is(msg.stdout, expectedOutput);
-  fs.removeSync(__dirname + '/build/another');
-})
+  await kubozerConf(tmpDir, config);
 
+  const actual   = await execa.stdout(CLI, ['--build'], {cwd: tmpDir});
+  const expected = ('\n> Started STAGING build')
+                    .concat(warn('\n⚠️ WARNING: the "buildFolder" and the "webpackConfig.output.path" are not the same.'));
+
+  t.is(actual, expected);
+});
 
 test('do STAGING build without NODE_ENV declared', async t => {
-  const msg = (await execa(__dirname + '/../dist/cli.js', ['--build']));
-  t.is(msg.stdout, '\n> Started STAGING build');
-  let expectedOutput = chalk.green(figures.tick) + ' REPLACE: HTML content replaced correctly.\n';
-    expectedOutput += chalk.green(figures.tick) + ' BUILD: Build JS and HTML completed correctly.\n';
-    expectedOutput += chalk.green(figures.tick) + ' >> Building...\n';
-    expectedOutput += chalk.green(figures.tick) + ' Everything works with charme 🚀';
-  // t.is(msg.stderr, expectedOutput);
-})
+  const {tmpDir} = t.context;
+
+  await intoParent(tmpDir, 'kubozer.conf.js');
+
+  const actual = await execa.stdout(CLI, ['--build'], {cwd: tmpDir});
+
+  t.is(actual, '\n> Started STAGING build');
+});
 
 test('do PRODUCTION build with NODE_ENV declared', async t => {
-  const msg = (await execa.shell('NODE_ENV=production ' + __dirname + '/../dist/cli.js --build'));
-  t.is(msg.stdout, '\n> Started PRODUCTION build');
-  let expectedOutput = chalk.green(figures.tick) + ' REPLACE: HTML content replaced correctly.\n';
-    expectedOutput += chalk.green(figures.tick) + ' BUILD: Build JS and HTML completed correctly.\n';
-    expectedOutput += chalk.green(figures.tick) + ' MINIFY: Minify JS and CSS completed correctly.\n';
-    expectedOutput += chalk.green(figures.tick) + ' >> Building...\n';
-    expectedOutput += chalk.green(figures.tick) + ' Everything works with charme 🚀';
-  // t.is(msg.stderr, expectedOutput);
-})
+  const {tmpDir} = t.context;
+
+  await intoParent(tmpDir, 'kubozer.conf.js');
+
+  const actual = await execa.stdout(CLI, ['--build'], {cwd: tmpDir, env: {NODE_ENV: 'production'}});
+
+  t.is(actual, '\n> Started PRODUCTION build');
+});
 
 test('do build and remove workspace correctly', async t => {
-  const msg = (await execa(__dirname + '/../dist/cli.js', ['--build'])).stdout
-  t.false(fs.existsSync(__dirname + '/workspace'))
-})
+  const {tmpDir} = t.context;
+
+  await intoParent(tmpDir, 'kubozer.conf.js');
+
+  await execa(CLI, ['--build'], {cwd: tmpDir});
+
+  t.false(await fs.pathExists(path.join(tmpDir, 'workspace')));
+});
 
 test('do BUMP', async t => {
-  const msg = (await execa(__dirname + '/../dist/cli.js', ['--bump', 'major'])).stderr;
-  let expectedOutput = chalk.green(figures.tick) + ' Bump from 3.0.0 to 4.0.0 completed.';
-  t.is(msg, expectedOutput);
-})
+  const {tmpDir} = t.context;
+
+  await intoParent(tmpDir, 'kubozer.conf.js');
+
+  const actual   = await execa.stderr(CLI, ['--bump', 'major'], {cwd: tmpDir});
+  const expected = `${green(figures.tick)} Bump from 2.0.0 to 3.0.0 completed.`;
+
+  t.is(actual, expected);
+});
 
 test('show help command when arg is not passed', async t => {
-  const msg = (await execa(__dirname + '/../dist/cli.js'));
-  let expectedOutput = '\n  Contactlab build utility\n';
-    expectedOutput += '\n  Usage\n    $ [NODE_ENV=env_name] kubozer [command]\n';
-    expectedOutput += '\n  Options\n  ';
-    expectedOutput += '  --build    Run the build task\n  ';
-    expectedOutput += '  --bump     Semver label for version bump: patch, minor, major, prepatch, preminor, premajor, prerelease\n  ';
-    expectedOutput += '  --i18n     Use I18N capabilities\n  ';
-    expectedOutput += '  --upload   Use ONLY with --i18n option: upload a translation file\n  ';
-    expectedOutput += '  --download Use ONLY with --i18n option: download a translation file\n';
-    expectedOutput += '\n  Examples\n    $ NODE_ENV=production kubozer --build\n    $ kubozer --bump minor\n  ';
-    expectedOutput += '  $ kubozer --i18n --upload en\n  ';
-    expectedOutput += '  $ kubozer --i18n --download it';
-  t.is(msg.stdout, expectedOutput);
-})
+  const {tmpDir} = t.context;
 
-test.afterEach.always(t => {
-  fs.removeSync('./test/build');
-  fs.copySync(__dirname + '/../kubozer.conf.js.backup', __dirname + '/../kubozer.conf.js');
-  fs.removeSync(__dirname + '/../kubozer.conf.js.backup');
+  await intoParent(tmpDir, 'kubozer.conf.js');
+
+  const actual   = await execa.stdout(CLI, {cwd: tmpDir});
+  const expected = ('\n  Contactlab build utility\n')
+    .concat('\n  Usage\n    $ [NODE_ENV=env_name] kubozer [command]\n')
+    .concat('\n  Options\n  ')
+    .concat('  --build    Run the build task\n  ')
+    .concat('  --bump     Semver label for version bump: patch, minor, major, prepatch, preminor, premajor, prerelease\n  ')
+    .concat('  --i18n     Use I18N capabilities\n  ')
+    .concat('  --upload   Use ONLY with --i18n option: upload a translation file\n  ')
+    .concat('  --download Use ONLY with --i18n option: download a translation file\n')
+    .concat('\n  Examples\n    $ NODE_ENV=production kubozer --build\n    $ kubozer --bump minor\n  ')
+    .concat('  $ kubozer --i18n --upload en\n  ')
+    .concat('  $ kubozer --i18n --download it');
+
+  t.is(actual, expected);
+});
+
+test.afterEach.always(async t => {
+  await fs.remove(t.context.tmpDir);
 });
